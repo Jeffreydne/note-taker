1050195462974e3270b5bf29c3f64208f4dd8adb
Jeffreydne <Jeffreydne@gmail.com>
2024-01-21 23:22:21 -0800

    attempt to make DELETE request functional- so far not working

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,22 @@ const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 // GET Route for retrieving notes
 router.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
-
+// Delete Route to delete a note
+router.delete('/notes:id', (req,res) => {
+  // console.info(`${req.method} request received to delete a note`);
+  console.log(res);
+  return res;
+})
+// .then((res) => {
+//   if (!res.ok) {
+//     throw new Error(`HTTP error! Status: ${res.status}`);
+//   }
+// })
+// .catch((error) => {
+//   console.error('Error deleting object:', error.message);
+// });
 // POST Route for a new note
 router.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
@@ -25,7 +38,7 @@ router.post('/notes', (req, res) => {
         id: uuidv4(),
       };
   
-      readAndAppend(newNote, '../db/db.json');
+      readAndAppend(newNote, './db/db.json');
       res.json(`Note added successfully`);
     } else {
       res.error('Error in adding note');
